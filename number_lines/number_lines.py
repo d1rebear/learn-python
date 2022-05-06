1faa7e670b1e2fc95e581f2c46a45ba4bd3bf7ce
xodohtro <103849368+xodohtro@users.noreply.github.com>
2022-05-06 18:36:20 +0500

    remove redundant lines

--- number_lines/number_lines.py
+++ number_lines/number_lines.py
@@ -50,10 +50,7 @@ min_sum_index = line_sums_list.index(min_sum_num) + 1
 print(f'Наибольшая сумма в строке # {max_sum_index}: {max_sum_num}')
 print(f'Наименьшая сумма в строке # {min_sum_index}: {min_sum_num}')
 
-# even_index = 0
-# odd_index = 0
-# even_index_id = sublist[0]
-# odd_index_id = sublist[0]
+
 #список четных чисел
 for line_index, sublist in enumerate(numberlists_list):
     odd_num_list = []
@@ -77,80 +74,3 @@ for line_index, sublist in enumerate(numberlists_list):
     print(f'Четных чисел в % от общего количества чисел # {line_index + 1}: {percent_even_num}%')
     print(f'Нечетных чисел в % от общего количества чисел # {line_index + 1}: {percent_odd_num}%')
 
-# even_nums_list = list(map(lambda i: i % 2 == 0, numberlists_list))
-# print(even_nums_list)
-
-# for i in range (1,30):
-#     if i % 2== 0:
-#         print (str(i) + ' =четное')
-#     else:
-#         print (str(i) + ' =нечетное')
-
-
-#print(sum(numberlists_list[0]))
-
-#sum_numbers = []
-#number_id = sum(numberlists_list[0])
-#sum_numbers.append(sum(numberlists_list[0]))
-#print(sum_numbers)
-
-
-# for x in numberlists_list:
-#     sum_numbers.extend(x)
-# print(sum_numbers)
-# print(numbers)
-
-# for max_sum_numbers_str in sum_numbers):
-#     if max_sum_numbers_str > 0:
-#         number_id = max_sum_numbers_str
-#     index_of_var = numberlists_list.index(number_id)
-# print(number_id)
-
-
-
-# numbers_count_list.sort()
-#
-# print(f'Самая длинная строка под номером #{line_index + 1}: {numbers_count_list[-1]}')
-# print(numbers_count_list)
-
-# a = [18, 52, 23, 41, 32]
-#
-# #variable to store largest number
-# ln = a[0] if a else None
-#
-# #find largest number
-# for i in a:
-# 	if i>ln:
-# 		ln=i
-
-# print("Largest element is: ",ln)
-
-# вытаскиваем числа  из индексированной строки
-# сравниваем эти числа
-
-#for line_index, line in enumerate(lines)
-
-# lines = [
-#     "6,2,4,1,7",
-#     "3,1,6",
-#     "1"
-# ]
-#
-# lines_count = 0
-# for line_index, line in enumerate(lines):
-#     line = line.strip()
-#     if len(line) <= 0:
-#         continue
-#     lines_count += 1
-#     numbers_as_str = line.split(',')
-#     numbers = []
-#
-#     num_count = 0
-#     for num_as_str in numbers_as_str:
-#         num = int(num_as_str)
-#         numbers.append(num)
-#         num_count += 1
-#     if num_count < 2 or num_count > 10:
-#         exit(f'Count of numbers in line #{line_index} is out of range [2-10]')
-#     print(f'line #{line_index}: {numbers}')
-# print(f'lines_count: {lines_count}')
